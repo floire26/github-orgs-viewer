43bd9bd698ebdd65a277a35c2c9a924a759bd74c
Edward Alexander <edward.alexander@shopee.com>
2024-07-18 21:49:26 +0700

    feat: change element colors inside ProjectList

--- client/src/ProjectList.tsx
+++ client/src/ProjectList.tsx
@@ -9,16 +9,16 @@ interface ProjectListProps {
 const ProjectList: React.FC<ProjectListProps> = ({ projects, onSelectProject }) => {
   return (
     <div className='mx-4 flex flex-col place-items-center'>
-      <h2 className='m-4'>Projects</h2>
+      <h2 className='m-4 bold'>Projects</h2>
       <div className="grid grid-cols-3 gap-4">
         {projects.map((project) => (
           <div onClick={() => onSelectProject(project.name)} className="card bg-transparent text-primary-content w-96">
-            <button className="card-body flex place-items-center bg-black/30 ">
+            <button className="card-body flex place-items-center bg-black/30">
               <h2 className="card-title text-slate-400">{project.name}</h2>
-                  <div className='flex place-items-center h-1/3 rounded-lg ring-2 m-2 p-2 text-center text-lime-200'>
+                  <div className='flex place-items-center h-1/3 rounded-lg ring-2 m-2 p-2 text-center text-lime-100 ring-slate-400 w-full'>
                     <p className='italic'>{project.description === null ? "No description" : project.description} </p>
                   </div>
-                  <div className='flex flex-row place-items-center border-2 rounded-lg w-auto'>
+                  <div className='flex flex-row place-items-center rounded-lg w-full ring-2 ring-slate-400'>
                     <div className='flex flex-col text-amber-400 ring-2 ring-amber-200 place-items-center p-1 m-2 w-1/4 rounded-lg'>
                       <img src="./star.svg" height={50} width={50}/>
                       <div>{project.stargazers_count}</div>
@@ -36,7 +36,7 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects, onSelectProject }) 
                       <div>{project.watchers_count}</div>
                     </div>
                   </div>
-                  <div className="justify-self-end ring-2 m-2 p-2 rounded-lg text-sky-300 ring-white">
+                  <div className="justify-self-end ring-2 m-2 p-2 rounded-lg text-sky-300 ring-slate-400 w-full">
                     <div>Updated At: {new Date(project.updated_at).toDateString()}</div>
                     <div>Created At: {new Date(project.created_at).toDateString()}</div>
                   </div>
