6bedd4a5f78dc065884951be4b41af8a407abd03
Edward Alexander <edward.alexander@shopee.com>
2024-07-18 21:00:37 +0700

    feat: added icons to and used it in ProjectList

--- client/src/ProjectList.tsx
+++ client/src/ProjectList.tsx
@@ -14,9 +14,11 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects, onSelectProject }) 
         {projects.map((project) => (
           <div onClick={() => onSelectProject(project.name)} className="card bg-primary text-primary-content w-96">
             <button className="card-body flex place-items-center">
-              <h2 className="card-title">{project.name}</h2>
-                  <div>{project.description === null ? "No description" : project.description}</div>
-                  <div className='flex flex-row place-items-center border-2 rounded-lg'>
+              <h2 className="card-title text-slate-400">{project.name}</h2>
+                  <div className='flex place-items-center h-1/3 rounded-lg ring-2 m-2 p-2 text-center text-lime-200'>
+                    <p className='italic'>{project.description === null ? "No description" : project.description} </p>
+                  </div>
+                  <div className='flex flex-row place-items-center border-2 rounded-lg w-auto'>
                     <div className='flex flex-col text-amber-400 ring-2 ring-amber-200 place-items-center p-1 m-2 w-1/4 rounded-lg'>
                       <img src="./star.svg" height={50} width={50}/>
                       <div>{project.stargazers_count}</div>
@@ -34,7 +36,7 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects, onSelectProject }) 
                       <div>{project.watchers_count}</div>
                     </div>
                   </div>
-                  <div className="justify-self-end">
+                  <div className="justify-self-end ring-2 m-2 p-2 rounded-lg text-sky-300 ring-white">
                     <div>Updated At: {new Date(project.updated_at).toDateString()}</div>
                     <div>Created At: {new Date(project.created_at).toDateString()}</div>
                   </div>
